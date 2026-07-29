aba4df2f4a9adaed2837eeee44574a638a44e587
Daniel Daza <daniel.daza2209@gmail.com>
2026-07-29 08:37:00 -0500

    Actualización del menú del día: sopa de plátano, albóndigas napolitanas y espaguetis con pollo (miércoles)

--- index.js
+++ index.js
@@ -36,15 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // MENÚ DEL DÍA (ACTUALIZAR DIARIAMENTE AQUÍ)
     // ==========================================
     const menuDelDia = {
-        sopa: 'Arroz',
+        sopa: 'Plátano',
         principios: [
-            'Ensalada de atún',
-            'Garbanzos rancheros',
+            'Verduras en crema de leche',
+            'Espaguetis',
             'Frijoles caseros'
         ],
         acompanamientos: [
-            'Francesa casera',
-            'Torta de zanahoria',
+            'Arepuelas',
+            'Papa en salsa criolla',
             'Tajadas maduras'
         ]
     };
