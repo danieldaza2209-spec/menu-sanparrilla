6e85375974a2a37eeea2c828036db56a22aba4a1
Daniel Daza <daniel.daza2209@gmail.com>
2026-07-31 07:44:25 -0500

    Actualizar menú del día, especiales y tema verde olivo

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 applyTheme(savedTheme);
             } else {
                 // Predeterminado
-                applyTheme('campo-alegre');
+                applyTheme('olive-garden');
             }
         }
     };
@@ -36,15 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // MENÚ DEL DÍA (ACTUALIZAR DIARIAMENTE AQUÍ)
     // ==========================================
     const menuDelDia = {
-        sopa: 'Fideos',
+        sopa: 'Sancocho',
         principios: [
-            'Ensalada hawaiana',
-            'Arvejas caseras',
+            'Ensalada primaveral',
+            'Lentejas caseras',
             'Frijoles caseros'
         ],
         acompanamientos: [
-            'Puré de papa',
-            'Maduro al horno',
+            'Criollas doraditas',
+            'Torta de mazorca',
             'Tajadas maduras'
         ]
     };
