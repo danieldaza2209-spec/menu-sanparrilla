b4379a69c6b18bcb22a588febd7d4dfece0c8c46
Daniel Daza <daniel.daza2209@gmail.com>
2026-08-01 08:32:07 -0500

    Actualizar menú del día y platos de .000

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuDelDia = {
         sopa: 'Sancocho',
         principios: [
-            'Ensalada primaveral',
-            'Lentejas caseras',
-            'Frijoles caseros'
+            'Ensalada Waldorf',
+            'Frijoles caseros',
+            'Espaguetis'
         ],
         acompanamientos: [
-            'Criollas doraditas',
-            'Torta de mazorca',
+            'Francesas caseras',
+            'Maduro con bocadillo',
             'Tajadas maduras'
         ]
     };
