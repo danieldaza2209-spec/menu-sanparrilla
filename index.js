5081404b0224260ca5f13ddf204a55864681caf0
Daniel Daza <daniel.daza2209@gmail.com>
2026-08-02 09:15:19 -0500

    Actualizar principios y acompañamientos del día

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuDelDia = {
         sopa: 'Sancocho',
         principios: [
-            'Ensalada Waldorf',
+            'Ensalada tropical',
             'Frijoles caseros',
             'Espaguetis'
         ],
         acompanamientos: [
-            'Francesas caseras',
-            'Maduro con bocadillo',
+            'Yuca frita',
+            'Yuca al vapor',
             'Tajadas maduras'
         ]
     };
